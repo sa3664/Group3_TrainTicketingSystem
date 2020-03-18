b8a436cf4ce5604b58fa31cdb3414fbd27b7006d
sa3664 <sa3664@drexel.edu>
2020-03-18 01:40:58 -0400

    Add Cart and Route

---
+++ src/main/java/edu/drexel/TrainDemo/PathFormatter.java
@@ -0,0 +1,24 @@
+package edu.drexel.TrainDemo;
+
+import java.math.BigInteger;
+import java.util.Locale;
+import java.text.ParseException;
+
+import org.springframework.format.Formatter;
+
+import edu.drexel.TrainDemo.models.Path;
+
+public class PathFormatter implements Formatter<Path> {
+
+    @Override
+    public String print(Path path, Locale locale) {
+		return path.getId();
+	}
+
+	@Override
+	public Path parse(String id, Locale locale) throws ParseException {
+		Path path = new Path();
+		path.setId(id);
+		return path;
+	}
+}
--- src/main/java/edu/drexel/TrainDemo/TrainDemoApplication.java
+++ src/main/java/edu/drexel/TrainDemo/TrainDemoApplication.java
@@ -2,10 +2,17 @@ package edu.drexel.TrainDemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class TrainDemoApplication {
+public class TrainDemoApplication implements WebMvcConfigurer{
     public static void main(String[] args) {
         SpringApplication.run(TrainDemoApplication.class, args);
+    }
+
+    @Override
+    public void addFormatters(FormatterRegistry formatterRegistry) {
+        formatterRegistry.addFormatter(new PathFormatter());
     }
 }
