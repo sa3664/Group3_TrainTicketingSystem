28bd17df355177f90de9b59b304b685cec65e8e7
sa3664 <sa3664@drexel.edu>
2020-03-21 15:13:59 -0400

    Search completed

--- src/main/java/edu/drexel/TrainDemo/controllers/SearchController.java
+++ src/main/java/edu/drexel/TrainDemo/controllers/SearchController.java
@@ -17,41 +17,49 @@ import org.springframework.web.bind.annotation.SessionAttributes;
 import edu.drexel.TrainDemo.models.SearchModel;
 import edu.drexel.TrainDemo.models.Stop;
 import edu.drexel.TrainDemo.models.CartItem;
+import edu.drexel.TrainDemo.models.Connection;
 import edu.drexel.TrainDemo.models.Path;
 //import edu.drexel.TrainDemo.repositories.RouteRepository;
 import edu.drexel.TrainDemo.repositories.StopRepository;
 import edu.drexel.TrainDemo.services.PathService;
 
 @Controller
-@SessionAttributes(value = {"directpaths"})
+@SessionAttributes(value = { "directpaths", "connectedpaths" })
 public class SearchController {
     private StopRepository stopRepository;
 
     @Autowired
     private PathService pathService;
-    
+
     @ModelAttribute("directpaths")
-public List<Path> populatePaths() {
-    return new ArrayList<Path>();
-}
+    public List<Path> populatePaths() {
+        return new ArrayList<Path>();
+    }
+
+    @ModelAttribute("connectedpaths")
+    public List<Path> populatecPaths() {
+        return new ArrayList<Path>();
+    }
+
     public SearchController(StopRepository stopRepository) {
         this.stopRepository = stopRepository;
     }
 
-    
-
     @RequestMapping("/Search")
     public String Search(@ModelAttribute SearchModel searchModel, Model model) {
-        Iterable<Stop> stops =  stopRepository.findAll();
+        Iterable<Stop> stops = stopRepository.findAll();
         Iterable<Path> directpaths = pathService.getDirectPaths(searchModel.getFromStop(), searchModel.getToStop());
+        Iterable<Path> connectedpaths = pathService.getConnectedPaths(searchModel.getFromStop(), searchModel.getToStop());
 
         Optional<Stop> sourceName = stopRepository.findById(searchModel.getFromStop());
         Optional<Stop> destinationName = stopRepository.findById(searchModel.getToStop());
         model.addAttribute("stops", stops);
         model.addAttribute("directpaths", directpaths);
+        model.addAttribute("connectedpaths", connectedpaths);
         model.addAttribute("source", sourceName.get().getName());
         model.addAttribute("destination", destinationName.get().getName());
         model.addAttribute("CartItem", new CartItem());
         return "SearchResult";
-        }
+    }
+
 }
