f5fcd819b0104515f2b6d0bba37f93f1c73519e6
sa3664 <sa3664@drexel.edu>
2020-03-21 00:55:26 -0400

    Payment implementation complete

--- src/main/java/edu/drexel/TrainDemo/configuration/SecurityConfiguration.java
+++ src/main/java/edu/drexel/TrainDemo/configuration/SecurityConfiguration.java
@@ -4,8 +4,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
