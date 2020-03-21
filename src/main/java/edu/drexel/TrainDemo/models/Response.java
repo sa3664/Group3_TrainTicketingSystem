f5c9405bac9f3f20a3da876fe71e2e296c60a3d3
sa3664 <sa3664@drexel.edu>
2020-03-21 00:57:02 -0400

    Search Result UI change

---
+++ src/main/java/edu/drexel/TrainDemo/models/Response.java
@@ -0,0 +1,43 @@
+package edu.drexel.TrainDemo.models;
+
+public class Response {
+    private boolean status;
+    private String details;
+
+    public Response() {
+        this.status = true; 
+    }
+
+    public Response(boolean status) {
+        this.status = status;
+    }
+
+    public Response(boolean status, String details) {
+        this.status = status;
+        this.details = details;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
+
+    @Override
+    public String toString() {
+        return "Response{" +
+                "status=" + status +
+                ", details='" + details + '\'' +
+                '}';
+    }
+}
